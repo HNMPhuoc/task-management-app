da739481df5d92be15e14324065161f9c8476d0c
HNMPhuoc <hoangnguyenph2003@gmail.com>
2025-09-12 17:20:35 +0700

    xóa thư viện không cần

--- server/src/server.js
+++ server/src/server.js
@@ -2,7 +2,6 @@ import { PORT } from './config/env.js';
 import { connectDB } from './config/db.js';
 import app from './app.js';
 import logger from './utils/logger.js';
-import cors from "cors";
 
 const start = async () => {
     await connectDB();
